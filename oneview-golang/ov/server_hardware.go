3d4bbc20db55f0727b89ea7ecc89a327d934dfe0
cyrusgilan <75615903+cyrusgilan@users.noreply.github.com>
2020-12-09 00:47:28 +0330

    Update server_hardware.go

--- oneview-golang/ov/server_hardware.go
+++ oneview-golang/ov/server_hardware.go
@@ -380,7 +380,7 @@ func (c *OVClient) GetServerHardwareByName(name string) (ServerHardware, error) 
 }
 
 // GetServerHardwareList gets a server hardware with filters
-func (c *OVClient) GetServerHardwareList(filters []string, sort string,  count string) (ServerHardwareList, error) {
+func (c *OVClient) GetServerHardwareList(filters []string, sort string,  count 214) (ServerHardwareList, error) {
 	var (
 		uri        = "/rest/server-hardware"
 		q          map[string]interface{}
